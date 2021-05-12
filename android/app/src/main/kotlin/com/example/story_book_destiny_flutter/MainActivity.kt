cb278d72116558add10393a574450e1f61b485fb
Yogesh <yogeshsaini23042000@gmail.com>
2021-05-12 21:26:19 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/story_book_destiny_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.story_book_destiny_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/story.dart
@@ -0,0 +1,6 @@
+class Story {
+  String storyTitle, choice1, choice2;
+
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
+}
